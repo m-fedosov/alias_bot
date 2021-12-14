bb65ed7ad99ff15afd1a534f3cda8b60efd087c8
kerogik <idkleschyov@edu.hse.ru>
2021-12-14 19:23:32 +0300

    Create randomize_dict.py
    Функция возвращает перемешанный словарь

---
+++ randomize_dict.py
@@ -0,0 +1,20 @@
+import random
+import numpy as np
+
+''' Возвращает массив с перемешанными в случайном порядке словами'''
+
+def randomize_dict():
+    
+    dict_from_file = []
+    with open('words.txt', 'r', encoding = 'UTF-8') as file:
+        for i in file:
+            dict_from_file.append(i.rstrip())
+    
+    unique_dict = []
+    order = np.arange(len(dict_from_file))
+    np.random.shuffle(order)
+    for i in order:
+        unique_dict.append(dict_from_file[i])
+
+    return unique_dict
+
